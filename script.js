11fde8c212bfe663fbf5d5e4f93f30b5803c1a24
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-09-20 09:55:36 +0200

    Fungerande meny i mobilvy

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 // Hämta variabler
+let sectionStart = document.getElementById("sectionStart");
 let menuBtn = document.getElementById("menuBtn");
 
 
 // Klick på menyknappen
 menuBtn.addEventListener("click", () => {
-    console.log("Klick på meny-knappen");
 
-    menuBtn.classList.toggle("menuOpen");
+    // Skapa meny
+    let openMenu = document.createElement("section");
+    openMenu.classList = "openMenu";
+
+    openMenu.innerHTML = `
+        <h1>Meny</h1>
+
+        <button id="closeMenuBtn">
+            <span id="span1"></span>
+            <span id="span2"></span>
+        </button>
+
+        <h2>Nyheter</h2>
+        <h2>Funktioner</h2>
+        <h2>Boka</h2>
+    `;
+
+    sectionStart.append(openMenu);
+
+    // Klick på kryss för att stänga meny
+    document.getElementById("closeMenuBtn").addEventListener("click", () => {
+        location.reload();
+    })
 })
